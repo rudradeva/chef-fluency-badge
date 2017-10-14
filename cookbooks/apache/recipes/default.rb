92da46bb008035122e8191eb16c5b6dc93fcd05e
Rudra Deva <rudradeva@gmail.com>
2017-10-14 19:22:58 +0530

    Added motd and changed apache defult for node attribute

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,7 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
 package 'apache' do
 	package_name 'httpd'
 	action :install
@@ -14,3 +18,7 @@ service 'apache' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
+#include_recipe 'apache::websites'
+
+
